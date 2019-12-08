5cbfc41a666d078f495ad81d46eba6cc4d37bf1e
FloydWatson <floydbcseit@gmail.com>
2019-12-08 23:16:38 +1300

    fixed nav bug. sell bug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,62 +1,62 @@
 <template>
-  <nav class="navbar navbar-dark bg-dark">
-    <div class="navbar-header">
-      <router-link to="/" class="navbar-brand">Stock Trader</router-link>
-    </div>
+<nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+            </div>
 
-    <div class="collapse navbar-collapse">
-      <ul class="navbar-nav nav">
-        <router-link tag="li" to="/portfolio" activeClass="active" class="nav-item">
-          <a>Portfolio</a>
-        </router-link>
-        <router-link tag="li" to="/stocks" activeClass="active" class="nav-item">
-          <a>Stocks</a>
-        </router-link>
-      </ul>
-      <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
-      <ul class="nav navbar-nav navbar-right">
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+                    <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+                </ul>
+                <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
+                <ul class="nav navbar-nav navbar-right">
         <li>
-          <a href="#" role="button" @click="endDay">End Day</a>
+          <a href="#" @click="endDay">End Day</a>
         </li>
-        <li class="dropdown">
+        <li
+          class="dropdown"
+        >
           <a
             href="#"
-            role="button"
-            data-toggle="dropdown"
             class="dropdown-toggle"
+            data-toggle="dropdown"
+            role="button"
             aria-haspopup="true"
             aria-expanded="false"
-          >Save / load</a>
+          >
+            Save & Load
+            <span class="caret"></span>
+          </a>
           <ul class="dropdown-menu">
             <li>
-              <a href="#">Save Data</a>
+              <a href="#" @click="saveData">Save Data</a>
             </li>
             <li>
-              <a href="#">Load Data</a>
+              <a href="#" @click="loadData">Load Data</a>
             </li>
           </ul>
         </li>
       </ul>
-    </div>
-  </nav>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+  
 </template>
 
 <script>
-import {mapActions} from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
-  
-
   computed: {
     funds() {
       return this.$store.getters.funds;
     }
   },
   methods: {
-    ...mapActions([
-      'randomizeStocks'
-    ]),
-    endDay(){
+    ...mapActions(["randomizeStocks"]),
+    endDay() {
       this.randomizeStocks();
     }
   }
